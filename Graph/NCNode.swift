2d8e209d78da7157ee659eef943b177e0fd690d0
nk <njck_g@yahoo.com>
2016-11-02 17:49:37 +0200

    Description update

--- Graph/NCNode.swift
+++ Graph/NCNode.swift
@@ -8,13 +8,10 @@
 
 import Foundation
 
-
-//#MARK: TODO rename `Vertex` to `Node`
-
 /**
- Class for vertex object.
+ Class for node object.
  Open class used in Graph ADT.
- `Vertex` object represents Node/Vertex in
+ `Node` object represents Node in
  directed or undirected graph.
  */
 public final class NCNode: NCNodeProtocol, Hashable {
@@ -45,51 +42,3 @@ public final class NCNode: NCNodeProtocol, Hashable {
     }
     
 }
-
-
-
-
-
-
-
-
-
-
-
-/*
-public struct NCNode:NCNodeProtocol, Hashable {
-
-    public typealias Name = String
-    
-    public var name: Name
-    //#MARK: TODO move score out from here
-    public var score: Int
-    
-    public init(name: Name) {
-        self.init(name: name, score: Int.max)
-    }
-    
-    public init(node: NCNode) {
-        self.init(name: node.name, score: node.score)
-    }
-    
-    init(name: Name, score: Int = Int.max) {
-        self.name = name
-        self.score = score
-    }
-    
-    public var hashValue: Int {
-        return name.hashValue
-    }
-    
-    public static func ==(lhs: NCNode, rhs: NCNode) -> Bool {
-        return lhs.name == rhs.name
-    }
-    
-    static public func <(lhs: NCNode, rhs: NCNode) -> Bool {
-        return lhs.score < rhs.score
-    }
-    
-    
-}
-*/
